ac6ca27415424a85e019e0eac567179b45ae2017
Byoung Jun Park <jk04150@gmail.com>
2022-05-15 21:03:50 +0900

    Revert "modify models.py"
    This reverts commit a86af71436a5f31db06082db06a701f31e551f35.

--- cloud_app/models.py
+++ cloud_app/models.py
@@ -1,21 +1,28 @@
 from djongo import models
-# from django import forms
-
-
 # Create your models here.
 
-# class Comment(models.Model):
-#     comment_name = models.CharField(max_length=50)
-#     comment_date = models.DateField()
-#     comment_text = models.TextField()
+class Owner(models.Model):
+    _id = models.ObjectIdField()
+    owner_name = models.CharField(max_length=50)
 
-#     class Meta:
-#         abstract = True
+    class Meta:
+        abstract = True
 
-# class CommentForm(forms.ModelForm):
-#     class Meta:
-#         model = Comment
-#         fields = ('comment_name', 'comment_date', 'comment_text')
+class Guest(models.Model):
+    _id = models.ObjectIdField()
+    guest_name = models.CharField(max_length=50)
+
+    class Meta:
+        abstract = True
+
+class Comment(models.Model):
+    _id = models.ObjectIdField()
+    comment_name = models.CharField(max_length=50)
+    comment_date = models.DateField()
+    comment_text = models.TextField()
+
+    class Meta:
+        abstract = True
 
 
 class UserInfo(models.Model):
@@ -25,20 +32,18 @@ class UserInfo(models.Model):
     images = models.TextField()
     phone_num = models.CharField(max_length=15)
 
-
 class FileDetailInfo(models.Model):
     _id = models.ObjectIdField()
     file_title = models.CharField(max_length=50)
     file_upload = models.DateField()
     file_images = models.TextField()
     file_url = models.FileField(upload_to='directory/')
-    owner_name = models.CharField(max_length=50)
-    guest_name = models.CharField(max_length=50)
-    comment_name = models.CharField(max_length=50)
-    comment_date = models.DateField()
-    comment_text = models.TextField()
-    # comment = models.EmbeddedField(
-    #     model_container=Comment,
-    #     model_form_class=CommentForm
-    # )
-
+    owner = models.EmbeddedField(
+        model_container=Owner
+    )
+    guest = models.EmbeddedField(
+        model_container=Guest
+    )
+    comment = models.EmbeddedField(
+        model_container=Comment
+    )
