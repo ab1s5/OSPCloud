b1ce7111a1b3db26031ccf7635b93de727c1e858
Byoung Jun Park <jk04150@gmail.com>
2022-05-15 20:56:36 +0900

    modify models.py and admin.py

--- cloud_app/admin.py
+++ cloud_app/admin.py
@@ -2,5 +2,4 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(UserInfo)
-admin.site.register(FileDetailInfo)
+admin.site.register([UserInfo, FileDetailInfo, Comment])
