a86af71436a5f31db06082db06a701f31e551f35
Byoung Jun Park <jk04150@gmail.com>
2022-05-15 18:15:46 +0900

    modify models.py
    delete EmbeddedField

--- cloud_app/models.py
+++ cloud_app/models.py
@@ -1,28 +1,21 @@
 from djongo import models
+# from django import forms
+
+
 # Create your models here.
 
-class Owner(models.Model):
-    _id = models.ObjectIdField()
-    owner_name = models.CharField(max_length=50)
+# class Comment(models.Model):
+#     comment_name = models.CharField(max_length=50)
+#     comment_date = models.DateField()
+#     comment_text = models.TextField()
 
-    class Meta:
-        abstract = True
+#     class Meta:
+#         abstract = True
 
-class Guest(models.Model):
-    _id = models.ObjectIdField()
-    guest_name = models.CharField(max_length=50)
-
-    class Meta:
-        abstract = True
-
-class Comment(models.Model):
-    _id = models.ObjectIdField()
-    comment_name = models.CharField(max_length=50)
-    comment_date = models.DateField()
-    comment_text = models.TextField()
-
-    class Meta:
-        abstract = True
+# class CommentForm(forms.ModelForm):
+#     class Meta:
+#         model = Comment
+#         fields = ('comment_name', 'comment_date', 'comment_text')
 
 
 class UserInfo(models.Model):
@@ -32,18 +25,20 @@ class UserInfo(models.Model):
     images = models.TextField()
     phone_num = models.CharField(max_length=15)
 
+
 class FileDetailInfo(models.Model):
     _id = models.ObjectIdField()
     file_title = models.CharField(max_length=50)
     file_upload = models.DateField()
     file_images = models.TextField()
     file_url = models.FileField(upload_to='directory/')
-    owner = models.EmbeddedField(
-        model_container=Owner
-    )
-    guest = models.EmbeddedField(
-        model_container=Guest
-    )
-    comment = models.EmbeddedField(
-        model_container=Comment
-    )
+    owner_name = models.CharField(max_length=50)
+    guest_name = models.CharField(max_length=50)
+    comment_name = models.CharField(max_length=50)
+    comment_date = models.DateField()
+    comment_text = models.TextField()
+    # comment = models.EmbeddedField(
+    #     model_container=Comment,
+    #     model_form_class=CommentForm
+    # )
+
