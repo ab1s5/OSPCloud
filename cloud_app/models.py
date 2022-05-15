b747d27b7cfeaea4d6e6478225aebc99286f94c7
Byoung Jun Park <jk04150@gmail.com>
2022-05-15 21:03:50 +0900

    Revert "modify models.py, add foms.py"
    This reverts commit 387a3647bc05585b51b540a210743e5d84ac62ad.

--- cloud_app/models.py
+++ cloud_app/models.py
@@ -1,11 +1,22 @@
 from djongo import models
-from django.utils import timezone
-
-
 # from django import forms
 
 
 # Create your models here.
+
+# class Comment(models.Model):
+#     comment_name = models.CharField(max_length=50)
+#     comment_date = models.DateField()
+#     comment_text = models.TextField()
+
+#     class Meta:
+#         abstract = True
+
+# class CommentForm(forms.ModelForm):
+#     class Meta:
+#         model = Comment
+#         fields = ('comment_name', 'comment_date', 'comment_text')
+
 
 class UserInfo(models.Model):
     _id = models.ObjectIdField()
@@ -18,36 +29,16 @@ class UserInfo(models.Model):
 class FileDetailInfo(models.Model):
     _id = models.ObjectIdField()
     file_title = models.CharField(max_length=50)
-    file_upload = models.DateField(default=timezone.now)
+    file_upload = models.DateField()
     file_images = models.TextField()
     file_url = models.FileField(upload_to='directory/')
     owner_name = models.CharField(max_length=50)
     guest_name = models.CharField(max_length=50)
-    # comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    # comment_name = models.CharField(max_length=50)
-    # comment_date = models.DateField()
-    # comment_text = models.TextField()
-    # comment = models.ArrayField(
+    comment_name = models.CharField(max_length=50)
+    comment_date = models.DateField()
+    comment_text = models.TextField()
+    # comment = models.EmbeddedField(
     #     model_container=Comment,
-    #     # model_form_class=CommentForm
+    #     model_form_class=CommentForm
     # )
 
-    def __str__(self):
-        return self.file_title
-
-
-class Comment(models.Model):
-    file_detail = models.ForeignKey(FileDetailInfo, related_name='comments', on_delete=models.CASCADE)
-    comment_name = models.CharField(max_length=50)
-    comment_date = models.DateField(default=timezone.now)
-    comment_text = models.TextField()
-    #     class Meta:
-    #     abstract = True
-
-    def __str__(self):
-        return self.comment_name
-
-# class CommentForm(forms.ModelForm):
-#     class Meta:
-#         model = Comment
-#         fields = ('comment_name', 'comment_date', 'comment_text')
