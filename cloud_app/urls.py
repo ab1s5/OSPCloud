0407e1d360d54059fd25a023477d69f0abb2a39d
abs <shlee259p@gmail.com>
2022-05-16 17:51:09 +0900

    Modify login logic; Implement logout

--- cloud_app/urls.py
+++ cloud_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.urls import include
 
 urlpatterns = [
-    path('index/', views.index, name='index')
+    path('index/', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.sign_up, name='signup'),
+    path('logout/', views.logout, name='logout'),
 ]
