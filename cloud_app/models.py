ceeb822a28e62c837b02654689e23204df075aa4
Byoung Jun Park <jk04150@gmail.com>
2022-05-17 11:11:52 +0900

    Update models.py

--- cloud_app/models.py
+++ cloud_app/models.py
@@ -1,52 +1,39 @@
-from djongo import models
-from django.utils import timezone
-from django.conf import settings
-
 # from django import forms
 
+from djongo import models
+from django.utils import timezone
 
 # Create your models here.
 
 class UserInfo(models.Model):
-    _id = models.ObjectIdField()
+    _id = models.ObjectIdField(unique=True)
     name = models.CharField(max_length=50)
-    email = models.CharField(max_length=30)
-    images = models.TextField()
+    email = models.CharField(primary_key=True, max_length=30, unique=True)
     phone_num = models.CharField(max_length=15)
+   
+    object = models.DjongoManager()
 
+    def __str__(self):
+        return self.name
 
 class FileDetailInfo(models.Model):
-    _id = models.ObjectIdField()
+    id = models.IntegerField(primary_key=True, unique=True)
     file_title = models.CharField(max_length=50)
     file_upload = models.DateField(default=timezone.now)
     file_url = models.FileField(upload_to='directory/')
-    owner_name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    guest_name = models.CharField(max_length=50)
-    # comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    # comment_name = models.CharField(max_length=50)
-    # comment_date = models.DateField()
-    # comment_text = models.TextField()
-    # comment = models.ArrayField(
-    #     model_container=Comment,
-    #     # model_form_class=CommentForm
-    # )
+    owner_name = models.ForeignKey('UserInfo', to_field='email', on_delete=models.CASCADE)
+    guest_name = models.CharField(max_length=50, null=True)
+
+    object = models.DjongoManager()
 
     def __str__(self):
         return self.file_title
 
 
 class Comment(models.Model):
-    file_detail = models.ForeignKey(FileDetailInfo, related_name='comments', on_delete=models.CASCADE)
-    comment_name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    file_detail = models.ForeignKey('FileDetailInfo', to_field='id', on_delete=models.CASCADE)
+    comment_name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     comment_date = models.DateField(default=timezone.now)
-    comment_text = models.TextField()
-    #     class Meta:
-    #     abstract = True
+    comment_text = models.TextField(null=False)
 
-    def __str__(self):
-        return self.comment_name
-
-# class CommentForm(forms.ModelForm):
-#     class Meta:
-#         model = Comment
-#         fields = ('comment_name', 'comment_date', 'comment_text')
+    object = models.DjongoManager()
